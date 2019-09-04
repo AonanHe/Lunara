bd9d21730ef231d3126d613f834978027696d6b0
AonanHe <aonanhe@hotmail.com>
2019-09-04 11:44:01 +0800

    Add examples to gcd and lcm

--- src/gcd.js
+++ src/gcd.js
@@ -3,10 +3,15 @@
  * which are not all zero.
  * The greatest common divisor g is the largest natural number
  * that divides both a and b without leaving a remainder.
+ *
  * @category Math
  * @param {number} a
  * @param {number} b
  * @return {number} Returns the GCD.
+ * @example
+ *
+ * gcd(18, 27)
+ * // => 9
  */
 function gcd(a, b) {
   return b === 0 ? a : gcd(b, a % b)
